0eec46c8565f8e20301f448fee65342c3925d07d
Ruslan Variushkin <ruslan@host4.biz>
2017-05-29 01:01:17 +0200

    initiall commit

--- ai-bolitisp5.py
+++ ai-bolitisp5.py
@@ -29,7 +29,14 @@ mode='2'
 memory='125M'
 size='2M'
 delay='500'
-
+# report file
+reportfile = "/tmp/ai-bolit-report.html"
+# work tmp file
+wtf = "/tmp/ai-bolit.log"
+# mail 
+serverport = "example.com:587"
+username = "info@example.com"
+password = "pass"
 
 def Checkwebdomain ():
     URLISP= urlISP+ "/ispmgr?authinfo="+userISP+":"+passwordISP+"&func=webdomain&out=xml"
@@ -80,12 +87,32 @@ def Mail(account):
                 #print accountBill.firstChild.nodeValue, " compare with  ",  account
                 for email in node.getElementsByTagName('email'):
                     return  email.firstChild.nodeValue 
+def sendmail(email):
+    import smtplib
+    from email.mime.multipart import MIMEMultipart
+    from email.mime.text import MIMEText
+    me = "info@host4.biz"
+    fname = "/tmp/ai-bolit_report.html"
+    with open(fname, "r") as myfile:
+        html=myfile.read()
+    msg = MIMEMultipart('utf-8') 
+    msg['Subject'] = 'The contents of'
+    msg['From'] = me
+    msg['To'] = email
+    part1 = MIMEText(html, 'html')
+    msg.attach(part1)
+    s = smtplib.SMTP(serverport)
+    s.starttls()
+    s.login(username,password)
+    s.sendmail(me, email, msg.as_string())
+    s.quit()
+    
+
 
 
 def Check(webpath, domain, email, user):
     if email is None:
         return None
-    MyPath=os.getcwd()
     datafile = file(skipfile)
     for line in datafile:
         if email in line:
@@ -93,8 +120,17 @@ def Check(webpath, domain, email, user):
         else:
             path=Pathweb + user+"/data/"+ webpath
             #print path
-            cmd="php %s --skip=%s --mode=%s --memory=%s --size=%s --delay=%s --report=%s --path=%s"%(aibolit,skip,mode,memory,size,delay,email,path)
+            cmd="php %s --skip=%s --mode=%s --memory=%s --size=%s --delay=%s --report=%s --path=%s > %s"%(aibolit,skip,mode,memory,size,delay,reportfile,path,wtf)
             os.system(cmd)
+            with open('/tmp/ai-bolit.log') as f:
+                last = None
+                for line in (line for line in f if line.rstrip('\n')):
+                    last = line
+            code=last.split()
+            if int(code[2]) == 2:
+                sendmail(email)
+            else:
+                pass
 
 def main():
     Checkwebdomain()
