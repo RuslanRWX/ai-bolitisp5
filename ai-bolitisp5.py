c11c219fc1c706f568c66c08d835e657365d238b
Ruslan Variushkin <ruslan@host4.biz>
2017-10-03 15:08:32 +0200

    initiall commit

--- ai-bolitisp5.py
+++ ai-bolitisp5.py
@@ -10,61 +10,6 @@ import config
 from config import *
 
 
-def Checkwebdomain():
-    URLISP = urlISP + "/ispmgr?authinfo=" + userISP + \
-        ":" + passwordISP + "&func=webdomain&out=xml"
-    res = urlopen(URLISP)
-    xmldoc = minidom.parse(res)
-    for node in xmldoc.getElementsByTagName('elem'):
-        #  print node.getElementsByTagName('name')
-        for name in node.getElementsByTagName('name'):
-            domain = name.firstChild.nodeValue
-            for owner in node.getElementsByTagName('owner'):
-                user = owner.firstChild.nodeValue
-                if len(sys.argv) > 1:
-                    if user == sys.argv[1]:
-                        for docroot in node.getElementsByTagName('docroot'):
-                            webpath = docroot.firstChild.nodeValue
-                            accountBill = Account(user)
-                            email = Mail(accountBill)
-                            print "Start Check, account Bill",  accountBill, "  Domain: " + domain + " Path:  " + webpath + " Email: ", email
-                            Check(webpath, domain, email, user)
-                            return
-                else:
-                    for docroot in node.getElementsByTagName('docroot'):
-                        webpath = docroot.firstChild.nodeValue
-                        accountBill = Account(user)
-                        email = Mail(accountBill)
-                        print "Start Check, account Bill",  accountBill, "  Domain: " + domain + "Path:  " + webpath + " Email: ", email
-                        Check(webpath, domain, email, user)
-
-
-def Account(user):
-    URLBILL = urlBill + "/billmgr?authinfo=" + \
-        userbill + ":" + passbill + "&func=vhost&out=xml"
-    res = urlopen(URLBILL)
-    xmldoc = minidom.parse(res)
-    for node in xmldoc.getElementsByTagName('elem'):
-        for usernameBill in node.getElementsByTagName('username'):
-            if usernameBill.firstChild.nodeValue == user:
-                for account in node.getElementsByTagName('account'):
-                    return account.firstChild.nodeValue
-
-
-def Mail(account):
-    URLBILL = urlBill + "/billmgr?authinfo=" + \
-        userbill + ":" + passbill + "&func=user&out=xml"
-    res = urlopen(URLBILL)
-    xmldoc = minidom.parse(res)
-    for node in xmldoc.getElementsByTagName('elem'):
-        for accountBill in node.getElementsByTagName('account'):
-            if accountBill.firstChild.nodeValue == account:
-                # print accountBill.firstChild.nodeValue, " compare with  ",
-                # account
-                for email in node.getElementsByTagName('email'):
-                    return email.firstChild.nodeValue
-
-
 def sendmail(email):
     import smtplib
     from email.mime.multipart import MIMEMultipart
@@ -82,6 +27,20 @@ def sendmail(email):
     s.login(username, password)
     s.sendmail(username, email, msg.as_string())
     s.quit()
+
+
+def Mail(account):
+    URLBILL = urlBill + "/billmgr?authinfo=" + \
+        userbill + ":" + passbill + "&func=user&out=xml"
+    res = urlopen(URLBILL)
+    xmldoc = minidom.parse(res)
+    for node in xmldoc.getElementsByTagName('elem'):
+        for accountBill in node.getElementsByTagName('account'):
+            if accountBill.firstChild.nodeValue == account:
+                # print accountBill.firstChild.nodeValue, " compare with  ",
+                # account
+                for email in node.getElementsByTagName('email'):
+                    return email.firstChild.nodeValue
 
 
 def Check(webpath, domain, email, user):
@@ -106,6 +65,53 @@ def Check(webpath, domain, email, user):
                 sendmail(email)
             else:
                 pass
+
+
+def print_user(docroot):
+    for docroot in docroot:
+    webpath = docroot.firstChild.nodeValue
+    accountBill = Account(user)
+    email = Mail(accountBill)
+    print "Start Check, account Bill", accountBill, \
+        "  Domain: "  + domain + \
+        " Path:  " + webpath + \
+        " Email: ", email
+    Check(webpath, domain, email, user)
+
+
+def Checkwebdomain():
+    URLISP = urlISP + "/ispmgr?authinfo=" + userISP + \
+        ":" + passwordISP + "&func=webdomain&out=xml"
+    res = urlopen(URLISP)
+    xmldoc = minidom.parse(res)
+    for node in xmldoc.getElementsByTagName('elem'):
+        #  print node.getElementsByTagName('name')
+        for name in node.getElementsByTagName('name'):
+            domain = name.firstChild.nodeValue
+            for owner in node.getElementsByTagName('owner'):
+                user = owner.firstChild.nodeValue
+                if len(sys.argv) > 1:
+                    if user == sys.argv[1]:
+                        print_user(node.getElementsByTagName('docroot'))
+                        return
+                    else:
+                        print "No such user"
+                else:
+                    print_user(node.getElementsByTagName('docroot'))
+
+
+def Account(user):
+    URLBILL = urlBill + "/billmgr?authinfo=" + \
+        userbill + ":" + passbill + "&func=vhost&out=xml"
+    res = urlopen(URLBILL)
+    xmldoc = minidom.parse(res)
+    for node in xmldoc.getElementsByTagName('elem'):
+        for usernameBill in node.getElementsByTagName('username'):
+            if usernameBill.firstChild.nodeValue == user:
+                for account in node.getElementsByTagName('account'):
+                    return account.firstChild.nodeValue
+
+
 
 
 def main():
