0acf5c4b5a9f2f4c2e88d8e35d8be6641368f8a8
Ruslan Variushkin <ruslan@host4.biz>
2017-10-03 14:48:17 +0200

    initiall commit

---
+++ config.py
@@ -0,0 +1,32 @@
+# config file ai-bolitisp5
+#  ISP url
+urlISP = 'https://ispURL:1500'
+# isp accesses, user/password
+userISP = 'root'
+passwordISP = 'pass'
+# local isp path , default is /var/www
+Pathweb = '/var/www/'
+# BILLmanager URL
+urlBill = 'https://billingURL'
+# Billmanager accesses , user/password
+userbill = 'userbilling'
+passbill = 'pass'
+# Ai-bolit script path
+aibolit = '/root/scripts/ai-bolit/ai-bolit.php'
+# path file of the skipe emails
+skipfile = "/root/scripts/ai-bolitisp5/skipemails.txt"
+# ai-bolit parameters
+skip = 'jpg,png,gif,jpeg,JPG,PNG,GIF,bmp,xml,zip,rar,css,avi,mov'
+mode = '2'
+memory = '125M'
+size = '2M'
+delay = '500'
+# report file
+reportfile = "/tmp/ai-bolit-report.html"
+# work tmp file
+wtf = "/tmp/ai-bolit.log"
+# mail
+Subject = "INFO"
+serverport = "example.com:587"
+username = "info@example.com"
+password = "pass"
