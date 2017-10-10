20fb15b3a322823f97a3c77c8f9b3e587e0a5564
Ruslan Variushkin <ruslan@host4.biz>
2017-10-10 15:25:15 +0200

    initiall commit

--- update.ai-bolit.sh
+++ update.ai-bolit.sh
@@ -2,7 +2,7 @@
 PATH=/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin:/root/bin
 
 path="/root/scripts/ai-bolit/"
-if [ ! -d "$path" ]; then { mkdir -p $path; } if
+if [ ! -d  $path ]; then { mkdir -p $path; } fi
 
 rm -f ${path}ai-bolit-hoster.zip
 wget -P ${path}  http://download.cloudscan.tech:28080/partners/ai-bolit-hoster.zip
